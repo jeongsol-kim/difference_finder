10d81f5f2f963f643c29b867eca1618b0191ca70
JeongsolKim <wjdthf3927@gmail.com>
2023-05-20 20:48:45 +0900

    fix demo

--- demo.py
+++ demo.py
@@ -1,37 +1,47 @@
-
+from pathlib import Path
 from PIL import Image
-import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.transforms import ToTensor
 from difference_finder.utils import to_np
 from difference_finder.finder import Finder
+from difference_finder.data import get_loader
 
 def main():
-    img1 = Image.open('samples/imgdir1/img2_1.png').convert('RGB')
-    img2 = Image.open('samples/imgdir2/img2_2.png').convert('RGB')
+    first_dir = Path('samples/imgdir1')
+    second_dir = Path('samples/imgdir2')
 
-    img1 = ToTensor()(img1).unsqueeze(0)[...,1:, 1:]
-    img2 = ToTensor()(img2).unsqueeze(0)[...,:-1,:-1]
+    # load all file paths
+    first_files = sorted(first_dir.glob('*.png'))
+    second_files = sorted(second_dir.glob('*.png'))
+    
+    # prepare dataloader
+    loader = get_loader(files1=first_files,
+                        files2=second_files,
+                        transform=ToTensor(),
+                        num_workers=0)
 
-    worker = Finder(pre_processor='identity', strategy='gradient', metric='psnr')
-    output = worker.run(img1, img2)
+    # define finder
+    worker = Finder(pre_processor='identity', strategy='gradient', metric='ssim')
 
-    plt.close()
-    plt.figure(figsize=(20,12))
-    plt.subplot(221)
-    plt.imshow(to_np(img1)[0])
-    plt.title('Image 1')
-    plt.subplot(222)
-    plt.imshow(to_np(img2)[0])
-    plt.title('Image 2')
-    plt.subplot(223)
-    plt.imshow(to_np(output)[0].mean(axis=-1), cmap='inferno')
-    plt.title('Difference')
-    plt.subplot(224)
-    plt.imshow(to_np(img1)[0], alpha=0.5)
-    plt.imshow(to_np(output)[0].mean(axis=-1), cmap='inferno', alpha=0.5)
-    plt.title('Overlay')
-    plt.savefig('test.png')
+    for i, (img1, img2) in enumerate(loader):
+        output = worker.run(img1, img2)
+
+        plt.close()
+        plt.figure(figsize=(20,12))
+        plt.subplot(221)
+        plt.imshow(to_np(img1)[0])
+        plt.title('Image 1')
+        plt.subplot(222)
+        plt.imshow(to_np(img2)[0])
+        plt.title('Image 2')
+        plt.subplot(223)
+        plt.imshow(to_np(output)[0].mean(axis=-1), cmap='inferno')
+        plt.title('Difference')
+        plt.subplot(224)
+        plt.imshow(to_np(img1)[0], alpha=0.5)
+        plt.imshow(to_np(output)[0].mean(axis=-1), cmap='inferno', alpha=0.5)
+        plt.title('Overlay')
+        plt.savefig(f'test_{i}.png')
 
 
 
